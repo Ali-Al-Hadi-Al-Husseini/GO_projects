96c4b9a62d138c48d301632300d3f24fd8e5ead0
Ali Al  Hadi Al Husseini <alihadi.alhusseini@gmail.com>
2025-06-02 22:56:19 +0300

    added lsitening

--- 1-Chat-Room-Server/main.go
+++ 1-Chat-Room-Server/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"net"
+)
+
 func main() {
+	fmt.Println(("server Started"))
+	listen, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer listen.Close()
 
 }
