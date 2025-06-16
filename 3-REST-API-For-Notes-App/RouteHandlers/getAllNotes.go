a805a299466b19b094fc2674a710e72b5bbd637c
Ali Al  Hadi Al Husseini <alihadi.alhusseini@gmail.com>
2025-06-16 23:49:48 +0300

    added query for rows

--- 3-REST-API-For-Notes-App/RouteHandlers/getAllNotes.go
+++ 3-REST-API-For-Notes-App/RouteHandlers/getAllNotes.go
@@ -1,5 +1,17 @@
 package handlers
 
-func getAllNotes() {
+import (
+	"net/http"
 
+	database "github.com/Ali-Al-Hadi-Al-Husseini/GO_projects/tree/main/3-REST-API-For-Notes-App/DB_Dir"
+	"github.com/gin-gonic/gin"
+)
+
+func getAllNotes(c *gin.Context) {
+	rows, err := database.DB.Query("SELECT id, tittle, content, created_at, updated_at from notes")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to query notes"})
+	}
+
+	defer rows.Close()
 }
