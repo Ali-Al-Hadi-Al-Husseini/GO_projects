5fb6b8d3a32ca01b8d92c818cacc662300eddd00
Ali Al  Hadi Al Husseini <alihadi.alhusseini@gmail.com>
2025-06-16 18:38:48 +0300

    changed name to avoid case-insensitive import collision:

---
+++ 3-REST-API-For-Notes-App/DB_Dir/database.go
@@ -0,0 +1,36 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var DB *sql.DB
+
+func InitDB() {
+	var err error
+
+	DB, err = sql.Open("sqlite3", "./notes.db")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	createTable := `
+	CREATE TABLE IF NOT EXISTS notes (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		title TEXT,
+		content TEXT,
+		created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+		updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+	);`
+	_, err = DB.Exec(createTable)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func init() {
+	InitDB()
+}
