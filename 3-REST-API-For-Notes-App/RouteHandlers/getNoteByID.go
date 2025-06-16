757c7bd1662d3f38e1e36f3cec7576d3ac274687
Ali Al  Hadi Al Husseini <alihadi.alhusseini@gmail.com>
2025-06-17 01:58:34 +0300

    added func fucntionality

--- 3-REST-API-For-Notes-App/RouteHandlers/getNoteByID.go
+++ 3-REST-API-For-Notes-App/RouteHandlers/getNoteByID.go
@@ -1,9 +1,32 @@
 package handlers
 
 import (
+	"database/sql"
+	"net/http"
+	"strconv"
+
+	database "github.com/Ali-Al-Hadi-Al-Husseini/GO_projects/tree/main/3-REST-API-For-Notes-App/DB_Dir"
 	"github.com/gin-gonic/gin"
 )
 
 func GetlNoteByID(c *gin.Context) {
+	id := c.Param("id")
+	var note database.Note
+
+	noteId, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid note ID"})
+	}
+
+	err = database.DB.QueryRow("SELECT id, title, content, created_at, updated_at FROM notes WHERE id = ?", noteId).
+		Scan(&note.ID, &note.Title, &note.Content, &note.CreatedAt, &note.UpdatedAt)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Note not found"})
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+		return
+	}
 
 }
