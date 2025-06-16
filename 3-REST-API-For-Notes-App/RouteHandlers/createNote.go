5588b3828350a66d6a1f6db0de51d1e84f4cc39f
Ali Al  Hadi Al Husseini <alihadi.alhusseini@gmail.com>
2025-06-16 03:53:32 +0300

    added createNote file and handlers dicrctorty

---
+++ 3-REST-API-For-Notes-App/RouteHandlers/createNote.go
@@ -0,0 +1,42 @@
+package createNote
+
+import (
+	"net/http"
+
+	database "github.com/Ali-Al-Hadi-Al-Husseini/GO_projects/tree/main/3-REST-API-For-Notes-App/Database"
+	"github.com/gin-gonic/gin"
+)
+
+func createNote(c *gin.Context) {
+	var note struct {
+		Title   string `json:"title"`
+		Content string `json:"content"`
+	}
+
+	err := c.ShouldBindJSON(&note)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON"})
+		return
+	}
+
+	statment, err := database.DB.Prepare("INSERT INTO notes (title, content) VALUES (?,?)")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	res, err := statment.Exec(note.Title, note.Content)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	id, _ := res.LastInsertId()
+
+	c.JSON(http.StatusCreated, gin.H{
+		"id":      id,
+		"title":   note.Title,
+		"content": note.Content,
+	})
+
+}
