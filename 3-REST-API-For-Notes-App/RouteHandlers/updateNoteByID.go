a462fc4ccfac0d55601f84a4604036f1d849c34b
Ali Al  Hadi Al Husseini <alihadi.alhusseini@gmail.com>
2025-06-17 02:11:58 +0300

    converted id to int

--- 3-REST-API-For-Notes-App/RouteHandlers/updateNoteByID.go
+++ 3-REST-API-For-Notes-App/RouteHandlers/updateNoteByID.go
@@ -1,9 +1,20 @@
 package handlers
 
 import (
+	"net/http"
+	"strconv"
+
+	database "github.com/Ali-Al-Hadi-Al-Husseini/GO_projects/tree/main/3-REST-API-For-Notes-App/DB_Dir"
 	"github.com/gin-gonic/gin"
 )
 
 func UpdateNoteById(c *gin.Context) {
+	id := c.Param("id")
+	var note database.Note
+
+	noteId, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid id"})
+	}
 
 }
