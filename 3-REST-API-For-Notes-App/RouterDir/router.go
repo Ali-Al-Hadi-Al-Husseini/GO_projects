df3e8ec1837b5c734ca6e0d404b17d7cb0d126a3
Ali Al  Hadi Al Husseini <alihadi.alhusseini@gmail.com>
2025-06-16 22:46:08 +0300

    changed package to the new name

--- 3-REST-API-For-Notes-App/RouterDir/router.go
+++ 3-REST-API-For-Notes-App/RouterDir/router.go
@@ -1,13 +1,13 @@
 package router
 
 import (
-	createNote "github.com/Ali-Al-Hadi-Al-Husseini/GO_projects/tree/main/3-REST-API-For-Notes-App/RouteHandlers"
+	handlers "github.com/Ali-Al-Hadi-Al-Husseini/GO_projects/tree/main/3-REST-API-For-Notes-App/RouteHandlers"
 	"github.com/gin-gonic/gin"
 )
 
 func RunRouter() {
 	router := gin.Default()
-	router.POST("note", createNote.CreateNote)
+	router.POST("note", handlers.CreateNote)
 
 	router.Run()
 }
