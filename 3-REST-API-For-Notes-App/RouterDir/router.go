516b78d0961d3b648cf7a26420811643351c2ead
Ali Al  Hadi Al Husseini <alihadi.alhusseini@gmail.com>
2025-06-16 22:43:32 +0300

    removed uncessary func

--- 3-REST-API-For-Notes-App/RouterDir/router.go
+++ 3-REST-API-For-Notes-App/RouterDir/router.go
@@ -5,10 +5,9 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func runRouter() {
+func RunRouter() {
 	router := gin.Default()
-	router.POST("note", func(c *gin.Context) {
-		createNote.CreateNote(c)
-	})
+	router.POST("note", createNote.CreateNote)
+
 	router.Run()
 }
