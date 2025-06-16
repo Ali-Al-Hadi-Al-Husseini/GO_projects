beca58d438540cd11a4ec068ff1f790e25feac0b
Ali Al  Hadi Al Husseini <alihadi.alhusseini@gmail.com>
2025-06-17 02:27:03 +0300

    parsed id and converted it to int

--- 3-REST-API-For-Notes-App/RouteHandlers/deleteNoteById.go
+++ 3-REST-API-For-Notes-App/RouteHandlers/deleteNoteById.go
@@ -1,7 +1,18 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
 
 func DeleteNoteById(c *gin.Context) {
+	id := c.Param("id")
 
+	noteId, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid id"})
+		return
+	}
 }
