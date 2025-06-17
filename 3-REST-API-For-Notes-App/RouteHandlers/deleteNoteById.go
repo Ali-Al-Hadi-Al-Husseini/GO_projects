03ef6090960bc9d7091e3b94376becef3f3d7a09
Ali Al  Hadi Al Husseini <alihadi.alhusseini@gmail.com>
2025-06-17 08:04:05 +0300

    prepared and excuted delete sql  statment

--- 3-REST-API-For-Notes-App/RouteHandlers/deleteNoteById.go
+++ 3-REST-API-For-Notes-App/RouteHandlers/deleteNoteById.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"strconv"
 
+	database "github.com/Ali-Al-Hadi-Al-Husseini/GO_projects/tree/main/3-REST-API-For-Notes-App/DB_Dir"
 	"github.com/gin-gonic/gin"
 )
 
@@ -15,4 +16,16 @@ func DeleteNoteById(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid id"})
 		return
 	}
+	statement, err := database.DB.Prepare("DELETE FROM notes Where id= ?")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"Error": "incorrect id"})
+		return
+	}
+	defer statement.Close()
+	_, err = statement.Exec(noteId)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"Error": "failed to excute querrty"})
+		return
+	}
+	c.JSON(http.StatusAccepted, gin.H{"deleted": "true"})
 }
