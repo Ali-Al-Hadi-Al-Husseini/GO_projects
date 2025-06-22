fe458f7407d023fb67b28161762bcfc546ba06ce
Ali Al  Hadi Al Husseini <alihadi.alhusseini@gmail.com>
2025-06-22 15:12:46 +0300

    added table exist test

--- 3-REST-API-For-Notes-App/tests/initDB_test.go
+++ 3-REST-API-For-Notes-App/tests/initDB_test.go
@@ -11,10 +11,21 @@ func Test_initDB(t *testing.T) {
 	os.Remove("notes.db")
 	database.InitDB()
 
+	// check if db exists
 	_, err := os.Stat("notes.db")
 	if os.IsNotExist(err) {
 		t.Fatal("Databse not created")
 
 	}
 
+	// check if table exists
+	row := database.DB.QueryRow("SELECT name FROM sqlite_master WHERE type='table' AND name='notes'")
+	var tableName string
+	if err := row.Scan(&tableName); err != nil || tableName != "notes" {
+		t.Fatal("Table 'notes' was not created")
+	}
+
+	// Cleanup
+	database.DB.Close()
+	os.Remove("notes.db")
 }
